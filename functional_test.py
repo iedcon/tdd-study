041bb91337e07fa121e784099fd6e64ef704b88b
iedcon <iedcon12@gmail.com>
2021-04-29 11:10:11 +0900

    작업 아이템 추가 가능여부 기능테스트

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,8 +16,30 @@ class NewVisitorTest(unittest.TestCase):
         # 웹 사이트 확인
         self.browser.get('http://localhost:8000')
 
-        # 웹 페이지 타이틀이 'To-Do'를 표시하고 있다.
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #작업을 추가하자!
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        #"공작깃털 사기"라고 입력한다.
+        inputbox.send_keys('공작깃털 사기')
+
+        # 엔터키를 치면 페이지가 갱신되고 작업목록에 1: 공작깃털 사기"가 추가된다.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 
