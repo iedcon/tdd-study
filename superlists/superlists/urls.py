0ec395170bd65cc5cfca799dcbf0d2d2b55ac4b4
iedcon <iedcon12@gmail.com>
2021-04-29 18:40:10 +0900

    폼 URL 변경, 신규 테스트 클래스, 목록 추가 뷰 및 URL, 리팩토링

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -20,5 +20,6 @@ import lists.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lists.views.home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', lists.views.view_list, name='view_list')
+    path('lists/the-only-list-in-the-world/', lists.views.view_list, name='view_list'),
+    path('lists/new/', lists.views.new_list, name='new_list'),
 ]
