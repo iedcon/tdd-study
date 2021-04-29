7f1e407e953443ba4b304fefad7ed1cfe2304b03
iedcon <iedcon12@gmail.com>
2021-04-29 09:33:59 +0900

    기능테스트에 uniitest 모듈 적용

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'The install' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 웹 사이트 확인
+        self.browser.get('http://localhost:8000')
+
+        # 웹 페이지 타이틀이 'To-Do'를 표시하고 있다.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == "__main__":
+    unittest.main()
